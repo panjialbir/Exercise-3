19a1d99a08a8a3388c4938ecca931130205a1e89
panjialbir <panjialbir10@gmail.com>
2022-11-24 16:02:03 +0700

    a

--- Exercise 3/Program.cs
+++ Exercise 3/Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Exercise_Linked List_A 
+    {
+    class Node
+{
+    //create nodes from the circular nexted list
+    public int rollNumber;
+    public string name;
+    public Node text;
+}
+class circularlist()
+    {
+    Node LAST;
+public circularlist()
+{
+    LAST = null;
+        }
+public bool Search(int rollNo, ref Node previous, ref Node current)/
+* Searches for the specified node*/
+    {
+    for (previous = current = LAST.next; current != LAST; previous
+current, current = current.next)
+    {
+    if (rollNo == current.rollNumber)
+return (true);/*returns true if the node is found*/
+}
+if (rollNo == LAST.rollNumber)/*If the node is present at the end*/
+    return true;
+else
+    return (false);/*return false if the node is not found*/
+
+}
+public bool listEmpty()
+{
+    if (LAST == null)
+        return true;
+    else
+        return false;
+
+}
+public void traverse()/*traverses all the nodes of the list*/
+{
+    if (listEmpty())
+        Console.WriteLine("\nList is empty");
+    else
+    {
+        Console.WriteLine("\nrecord in the list are: \n");
+        Node currentNode;
+        currentNode = LAST.next;
+        while (currentNode != LAST)
+        {
+            Console.Write(currentNode.rollNumber + "  " + currentNode.name + "\n");
+            currentNode = currentNode.next;
+        }
+        Console.Write(LAST.rollNumber + " ")
+    }
+}
+
+}
